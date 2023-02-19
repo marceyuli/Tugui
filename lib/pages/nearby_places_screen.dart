e8d88c30ad0748cf8a569691ce92a0b6f6ea60c4
Yuliana Montaño <yulianamarcela200@gmail.com>
2023-02-19 16:40:12 -0400

    Lugares cercanos con ubicacion real

--- lib/pages/nearby_places_screen.dart
+++ lib/pages/nearby_places_screen.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 
 import 'package:http/http.dart' as http;
 
@@ -14,12 +15,50 @@ class NearByPlacesScreen extends StatefulWidget {
 }
 
 class _NearByPlacesScreenState extends State<NearByPlacesScreen> {
-
+  Position? _currentPosition;
   String apiKey = "AIzaSyDF2wZV31k2clz5HlF8kJf7OHoiZJHWj_w";
   String radius = "30";
 
-  double latitude = 31.5111093;
-  double longitude = 74.279664;
+  Future<bool> _handleLocationPermission() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+          content: Text(
+              'Location services are disabled. Please enable the services')));
+      return false;
+    }
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        ScaffoldMessenger.of(context).showSnackBar(
+            const SnackBar(content: Text('Location permissions are denied')));
+        return false;
+      }
+    }
+    if (permission == LocationPermission.deniedForever) {
+      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+          content: Text(
+              'Location permissions are permanently denied, we cannot request permissions.')));
+      return false;
+    }
+    return true;
+  }
+
+  Future<void> _getCurrentPosition() async {
+    final hasPermission = await _handleLocationPermission();
+
+    if (!hasPermission) return;
+    await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high)
+        .then((Position position) {
+      setState(() => _currentPosition = position);
+    }).catchError((e) {
+      debugPrint(e);
+    });
+  }
 
   NearbyPlacesResponse nearbyPlacesResponse = NearbyPlacesResponse();
 
@@ -30,19 +69,17 @@ class _NearByPlacesScreenState extends State<NearByPlacesScreen> {
         title: const Text('Nearby Places'),
         centerTitle: true,
       ),
-
       body: SingleChildScrollView(
         child: Column(
           children: [
-            ElevatedButton(onPressed: (){
-
-              getNearbyPlaces();
-
-            }, child: const Text("Get Nearby Places")),
-
-            if(nearbyPlacesResponse.results != null)
-              for(int i = 0 ; i < nearbyPlacesResponse.results!.length; i++)
-                nearbyPlacesWidget(nearbyPlacesResponse.results![i])  
+            ElevatedButton(
+                onPressed: () {
+                  getNearbyPlaces();
+                },
+                child: const Text("Get Nearby Places")),
+            if (nearbyPlacesResponse.results != null)
+              for (int i = 0; i < nearbyPlacesResponse.results!.length; i++)
+                nearbyPlacesWidget(nearbyPlacesResponse.results![i])
           ],
         ),
       ),
@@ -50,34 +87,45 @@ class _NearByPlacesScreenState extends State<NearByPlacesScreen> {
   }
 
   void getNearbyPlaces() async {
-
-    var url = Uri.parse('https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=' + latitude.toString() + ','
-    + longitude.toString() + '&radius=' + radius + '&key=' + apiKey
-    );
+    _getCurrentPosition();
+    double? latitude = _currentPosition?.latitude;
+    double? longitude = _currentPosition?.longitude;
+    var url = Uri.parse(
+        'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=' +
+            latitude.toString() +
+            ',' +
+            longitude.toString() +
+            '&radius=' +
+            radius +
+            '&key=' +
+            apiKey);
 
     var response = await http.post(url);
 
-    nearbyPlacesResponse = NearbyPlacesResponse.fromJson(jsonDecode(response.body));
+    nearbyPlacesResponse =
+        NearbyPlacesResponse.fromJson(jsonDecode(response.body));
 
     setState(() {});
-
   }
 
   Widget nearbyPlacesWidget(Results results) {
-
     return Container(
       width: MediaQuery.of(context).size.width,
-      margin: const EdgeInsets.only(top: 10,left: 10,right: 10),
+      margin: const EdgeInsets.only(top: 10, left: 10, right: 10),
       padding: const EdgeInsets.all(5),
-      decoration: BoxDecoration(border: Border.all(color: Colors.black),borderRadius: BorderRadius.circular(10)),
+      decoration: BoxDecoration(
+          border: Border.all(color: Colors.black),
+          borderRadius: BorderRadius.circular(10)),
       child: Column(
         children: [
           Text("Name: " + results.name!),
-          Text("Location: " + results.geometry!.location!.lat.toString() + " , " + results.geometry!.location!.lng.toString()),
+          Text("Location: " +
+              results.geometry!.location!.lat.toString() +
+              " , " +
+              results.geometry!.location!.lng.toString()),
           Text(results.openingHours != null ? "Open" : "Closed"),
         ],
       ),
     );
-
-  } 
+  }
 }
